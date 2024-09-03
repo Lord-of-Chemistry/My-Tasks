6cb3192f34abd8d78462a9906b914609a906457b
WittySage <wittysage143@gmail.com>
2024-09-03 23:56:43 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  const containerStyle = {
+    maxWidth: '400px',
+    margin: '0 auto',
+    padding: '20px',
+    backgroundColor: 'lightblue',
+    borderRadius: '10px',
+    textAlign: 'center',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
